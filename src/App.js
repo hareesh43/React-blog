a85e0e8933245f2d5df517373f380711e2b85b40
hareesh43 <hareeshgummani@gmail.com>
2021-01-19 11:28:49 +0530

    added blog list

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import Homepage from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Homepage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>My blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">create</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
