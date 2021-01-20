c8ddb16fce0953d9deab10b29b9fe41f25037f6d
hareesh43 <hareeshgummani@gmail.com>
2021-01-20 14:36:00 +0530

    added error caching functionality

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,13 +1,12 @@
-const BlogList = ({ blogs, title,handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
-    <h2>{title}</h2>
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>{blog.body}</p>
           <p>writter by {blog.author}</p>
-          <button onClick={()=> handleDelete(blog.id) }>Delete blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Homepage = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog=>blog.id !== id)
-    setBlogs(newBlogs)
-  };
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [isError,setIsError] = useState(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(" http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok)
+          {
+            throw Error('your request object not found !! ')
+          }  
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        })
+        .catch((err) => {
+          setIsError(err);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs here !!" handleDelete = {handleDelete}/>
+    {isError && <div>{isError}</div>}
+      {isPending && <div>Loading....</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs here !!" />}
     </div>
   );
 };
-
 export default Homepage;
