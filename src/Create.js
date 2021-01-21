8631c591b8c9095d4052e622220c8e84033740b7
hareesh43 <hareeshgummani@gmail.com>
2021-01-21 19:44:22 +0530

    add blog and redirect features

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,58 @@
+import { useState } from "react";
+import {useHistory} from 'react-router-dom';
+
+
 const Create = () => {
-    return (<div className="create">
-        <h2>Add new blog !!!</h2>
-    </div>  );
-}
- 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("abc");
+  const [ispeding, setIsPending] = useState(false);
+  const history = useHistory()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setIsPending(false);
+      history.push('/')
+    });
+  };
+  return (
+    <div className="create">
+      <h2>Add new blog !!!</h2>
+      <form onSubmit={handleSubmit}>
+        <label> Blog title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label> Blog body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label> Blog author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="abc">abc</option>
+          <option value="xyz">xyz</option>
+        </select>
+        {!ispeding && <button>Add blog</button>}
+        {ispeding && <button disabled>Adding ...</button>}
+      </form>
+    </div>
+  );
+};
+
 export default Create;
