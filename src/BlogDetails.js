a498ea056b538f36b317aef7e2d69b485e375dbc
hareesh43 <hareeshgummani@gmail.com>
2021-01-23 19:02:57 +0530

    added blog delete feature

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -6,6 +6,14 @@ const BlogDetails = () => {
   const { data: blog, isPending, error } = useFetch(
     "http://localhost:8000/blogs/" + id
   );
+  const history = useHistory();
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -15,6 +23,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>written by {blog.author}</p>
           <p>{blog.body}</p>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
